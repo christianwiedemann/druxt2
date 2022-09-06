d5f1816712474a03c1921326be205ecca091140f
Rafael Schally <rafael.schally@key-tec.de>
2022-09-06 09:54:02 +0200

    gitignore

--- packages/oauth/playground/.nuxt/imports.d.ts
+++
@@ -1,8 +0,0 @@
-export { useHead, useMeta } from '#head';
-export { isVue2, isVue3 } from 'vue-demi';
-export { useAsyncData, useLazyAsyncData, refreshNuxtData, defineNuxtComponent, useNuxtApp, defineNuxtPlugin, useRuntimeConfig, useState, useFetch, useLazyFetch, useCookie, useRequestHeaders, useRequestEvent, setResponseStatus, useRouter, useRoute, useActiveRoute, defineNuxtRouteMiddleware, navigateTo, abortNavigation, addRouteMiddleware, throwError, showError, clearError, isNuxtError, useError, createError, defineNuxtLink } from '#app';
-export { withCtx, withDirectives, withKeys, withMemo, withModifiers, withScopeId, onActivated, onBeforeMount, onBeforeUnmount, onBeforeUpdate, onDeactivated, onErrorCaptured, onMounted, onRenderTracked, onRenderTriggered, onServerPrefetch, onUnmounted, onUpdated, computed, customRef, isProxy, isReactive, isReadonly, isRef, markRaw, proxyRefs, reactive, readonly, ref, shallowReactive, shallowReadonly, shallowRef, toRaw, toRef, toRefs, triggerRef, unref, watch, watchEffect, isShallow, effect, effectScope, getCurrentScope, onScopeDispose, defineComponent, defineAsyncComponent, resolveComponent, getCurrentInstance, h, inject, nextTick, provide, useAttrs, useCssModule, useCssVars, useSlots, useTransitionState } from 'vue';
-export { useHttp, useLazyHttp } from '../../../../node_modules/@nuxtjs-alt/http/dist/runtime/composables';
-export { usePinia } from '../../../../node_modules/@pinia/nuxt/dist/runtime/composables';
-export { definePageMeta } from '../../../../node_modules/nuxt/dist/pages/runtime/composables';
-export { useLink } from 'vue-router';
--- packages/oauth/playground/.nuxt/types/middleware.d.ts
+++
@@ -1,7 +0,0 @@
-import type { NavigationGuard } from 'vue-router'
-export type MiddlewareKey = "auth"
-declare module "/home/rafael/projects/druxt3/node_modules/nuxt/dist/pages/runtime/composables" {
-  interface PageMeta {
-    middleware?: MiddlewareKey | NavigationGuard | Array<MiddlewareKey | NavigationGuard>
-  }
-}
